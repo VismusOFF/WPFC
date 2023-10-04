b0628095fd9da43deb7ac9fee0aaffcc84bba94e
Дмитрий <144954267+VismusOFF@users.noreply.github.com>
2023-10-04 15:01:59 +0300

    1
    1

---
+++ Приложение на WPF/FistAplicationZhigatch/FistAplicationZhigatch/pages/PageAutorization.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FistAplicationZhigatch.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAutorization.xaml
+    /// </summary>
+    public partial class PageAutorization : Page
+    {
+        public PageAutorization()
+        {
+            InitializeComponent();
+        }
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем введенный логин и пароль
+            string login = Txtlogin.Text;
+            string password = PsbPassword.Password;
+
+            // Проверяем, если учетные данные верны
+            if (Authenticate(login, password))
+            {
+                // Успешная аутентификация
+                MessageBox.Show("Аутентификация успешна!");
+                // Здесь вы можете перейти на другую страницу или выполнить другие действия.
+            }
+            else
+            {
+                // Неверный логин или пароль
+                MessageBox.Show("Неверный логин или пароль. Пожалуйста, попробуйте еще раз.");
+            }
+        }
+
+        private bool Authenticate(string login, string password)
+        {
+            // Replace with your actual authentication logic.
+            // For this example, we're using hardcoded values.
+            return login == "1" && password == "1";
+        }
+    }
+}
---
+++ Приложение на WPF/FistAplicationZhigatch/FistAplicationZhigatch/pages/pr1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FistAplicationZhigatch.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для pr1.xaml
+    /// </summary>
+    public partial class pr1 : Page
+    {
+        public pr1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(TxtX.Text);
+            double y = Convert.ToDouble(TxtY.Text);
+            double z = Convert.ToDouble(TxtZ.Text);
+
+            double a = Math.Log(Math.Pow(y, (-Math.Sqrt(Math.Abs(x))))) * (x - (y / 2)) + Math.Pow(Math.Sin(Math.Atan(z)), 2);
+
+            LstResult.Items.Add("ИСП21.1А ПР1 Вариант 5 Селянский Жигач");
+            LstResult.Items.Add($"x={x}");
+            LstResult.Items.Add($"y={y}");
+            LstResult.Items.Add($"z={z}");
+            LstResult.Items.Add($"Результат a={a}");
+        }
+
+        private void Btnpr2_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем экземпляр страницы
+            pr2 page = new pr2();
+
+            // Навигируемся на новую страницу
+            NavigationService.Navigate(page);
+        }
+    }
+}
